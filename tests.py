8902d76c0441b61e3d163bd08b45139d7a710a91
Ivo Wetzel <ivo.wetzel@googlemail.com>
2010-05-04 19:39:49 +0200

    Hm who forgot to update the project name in here? *looks around* Well let's blame gedit for that!

--- tests.py
+++ tests.py
@@ -1,18 +1,18 @@
 # -*- coding: UTF-8 -*-
-#  This file is part of twp.
+#  This file is part of twitter-text-python.
 #
-#  twp is free software: you can redistribute it and/or
+#  twitter-text-python is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  twp is distributed in the hope that it will be useful,
+#  twitter-text-python is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License along with
-#  twp. If not, see <http://www.gnu.org/licenses/>.
+#  twitter-text-python. If not, see <http://www.gnu.org/licenses/>.
 
 
 # twp - Unittests --------------------------------------------------------------
