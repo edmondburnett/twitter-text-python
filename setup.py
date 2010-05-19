34e2239963d6e7d57f9237d253278b25d319ce4d
abuchanan <alexbuchanan@abuchanan.local>
2010-05-20 03:52:11 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='twitter-text-python',
+    version='1.0',
+    description='Tweet parser and formatter',
+    long_description=open('README.rst').read(),
+    author='Ivo Wetzel',
+    author_email='',
+    url='http://github.com/abuchanan/twitter-text-python',
+    license='GPL',
+    packages=['ttp'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        # I don't know what exactly this means, but why not?
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
